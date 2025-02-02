ed0698a03b79d81ed17d815b4c516a21a7e4f1da
dalnikcc <dalnikcc@gmail.com>
2025-02-02 11:47:46 +0100

    change to mongoDB

--- models/product.js
+++ models/product.js
@@ -1,27 +1,65 @@
-const Sequelize = require('sequelize');
+const getDb = require('../util/database').getDb; // It's a function that returns the database connection. We're importing it here so that we can use it in the save method below.
+const mongodb = require('mongodb');
 
-const sequelize = require('../util/database');
+class Product {
+    constructor(title, imageUrl, price, description, id) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this._id = id ? new mongodb.ObjectId(id) : null;
+    }
 
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-});
+    save() {
+        const db = getDb();
+        let dbOp;
+        if (this._id) {
+            return db.collection('products').updateOne({ _id: _id }, { $set: this }).then(result => {
+                console.log(result);
+            }).catch(err => {
+                console.log(err);
+            });
+        } else {
+            dbOp = db.collection('products').insertOne(this);
+        }
+        return dbOp.collection('products').insertOne(this).then(result => {
+            console.log(result);
+        }).catch(err => {
+            console.log(err);
+        });
+        // return db.execute(
+        //     'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+        //     [this.title, this.price, this.imageUrl, this.description]
+        // );
+    }
+
+    static findById(prodId) {
+        const db = getDb();
+        return db.collection('products').find({ _id: new mongodb.ObjectId(prodId) }).next().then(product => {
+            console.log(product);
+            return product;
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+    static fetchAll() {
+        const db = getDb();
+        return db.collection('products').find().toArray().then(products => {
+            console.log(products);
+            return products;
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+    static deleteById(prodId) {
+        const db = getDb();
+        return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) }).then(result => {
+            console.log('Deleted');
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+}
 
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,8 +1,34 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
 
-module.exports = sequelize;
+let _db
+
+const mongoConnect = callback => {
+    MongoClient.connect('mongodb+srv://dalnikcc:bVw1e1JHoz1WMNZE@devices0.85mwk.mongodb.net/?retryWrites=true&w=majority&appName=devices0')
+        .then(client => {
+            console.log('Connected!');
+            _db = client.db();
+            callback();
+            // const db = client.db('devices0');
+            // db.collection('products').insertOne({
+            //     title: 'A Book',
+            //     price: 12.99
+            // });
+        }).catch(err => {
+            console.log(err);
+            throw err;
+        }); // const getDb = require('../util/database').getDb;
+
+}
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+// module.exports = mongoConnect;
